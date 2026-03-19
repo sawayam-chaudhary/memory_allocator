49da88fcc157a9d6078b4b58133beeadc886b713
sawayam-chaudhary <sawayamonly@gmail.com>
2026-03-19 23:55:11 +0530

    Implemented get_free_block search logic and free and in my malloc.

--- malloc.c
+++ malloc.c
@@ -16,6 +16,22 @@ struct header *getLastBlock(struct header *last){
     return last;
 }
 
+struct header *find_block(struct header **last, size_t size){
+    struct header *current = head;
+    while(current != NULL && !(current->free==0 && current->data>=size)){
+            *last=current;
+            current=current->next;
+            }
+    return current;
+}
+
+void free(void *ptr){
+    if(!ptr) return;
+
+    struct header *block = (struct header*) ptr -1;
+    block->free=1;
+}
+
 void *malloc(size_t size){
 
     if(size<=0){
@@ -31,9 +47,14 @@ void *malloc(size_t size){
         head=block;
     }else{
         struct header *last= getLastBlock(head);
+        block=find_block(&last, real_size);
+        if(block){
+            block->free = 0;
+        }else{
         block = sbrk(real_size + header_size);
         if(block == (void *) -1) return NULL;
         last->next = block;
+    }
     }
 
     block->size= real_size;
